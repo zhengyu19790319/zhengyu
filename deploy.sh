ce6b7b11ea85932316a313321e2e561ea9799f9d
zhengyu19790319 <71920981+zhengyu19790319@users.noreply.github.com>
2020-10-08 15:53:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -63,7 +63,7 @@ if [ ! -f "./config/v2ray" ]; then
     wget -O./upx https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$branch/config/upx
     chmod 700 ./upx
     ./upx -1 -o ./v2raynew ./v2ray
-    mv -rf ./v2raynew ./v2ray
+    mv ./v2raynew ./v2ray
     chmod 700 ./v2ray
     rm -rf ./upx
     
